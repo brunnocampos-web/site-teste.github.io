167d26befe61747af354044bd56049fa90dbc443
figurante <brunno.campos@escola.pr.gov.br>
2026-08-25 10:58:05 -0300

    Correct theme toggle and add text change feature
    Fix typos and improve theme toggle functionality.

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@
 
 const botaotema =document.getElementByld("botaoTema");
 function alterarTema(){document.body.classList.toggle("escuro");
+if(dicument.body.classList.toggle("escuro")){ 
+                       botaoTema.textContent="modoClaro";
+}else{botaoTema.textContent="modo escuro";
+     }
+                      }
+botaoTema.addEventListener("click",alternarTema);
+//===================//ALTERAR TEXTO//==================
+const botaoMensagem =document.getElementByld("botaoMensagem"); const mensagem =document.getElementByld("mensagem");
+function alterarTexto(){mensagem.textContent = "o texto foi alterado pelo javaScript!";
+                       }
